482b1e7c4b074f48a48b6c434288517cd250eed6
anziledge <113919049+anziledge@users.noreply.github.com>
2022-09-20 20:04:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from distutils.log import debug
 from flask import Flask, render_template, request
 import json
 import pyqrcode
+import png
 import time
-
+import os
+import webbrowser
 app = Flask(__name__)
+
+#pic_folder = os.path.join('static', 'images')
+#app.config['UPLOAD_FOLDER'] = pic_folder
+fileno = 10
 
 @app.route('/')
 def home():
@@ -16,30 +22,49 @@ def products():
     with open('storage/products.txt', 'r') as file:
         for line in file:
             pros.append(eval(line.strip()))
+    
+    price_list = [product['amount payable'] for product in pros]
+    length = len(price_list)
+    total_price = sum(price_list)
             
-    return render_template('products.html', products=pros)
+    return render_template('products.html', products=pros, number=length, total_price=total_price)
 
-@app.route('/cart', methods = ['POST', 'GET'])
+@app.route('/cart', methods = ['GET', 'POST'])
 def cart():
-    data = request.get_json()
-    with open('storage/products.txt', 'a') as outfile:
-        json.dump(data, outfile)
-        outfile.write('\n')
-    return 'hi'
+    if request.method == 'POST':
+        data = request.get_json()
+        with open('storage/products.txt', 'a') as outfile:
+            json.dump(data, outfile)
+            outfile.write('\n')
+        webbrowser.open('http://127.0.0.1:5000/products')
+    return 'go to products instead of cart'
 
 @app.route('/payment')
 def pay():
-    #price = 10
-    url = pyqrcode.create('www.instagram.com')
-    url.svg("myqr", scale=10)
+    global fileno
+    pros = []
+    with open('storage/products.txt', 'r') as file:
+        for line in file:
+            pros.append(eval(line.strip()))
+    price_list = [product['amount payable'] for product in pros]
+    total_price = sum(price_list)
     
-    return render_template('qr.html')
-
-
-
+    filename = str(fileno)
+    fileno += 1
+    price=str(total_price)
+    s = 'upi://pay?pa=abdul2hadi2k2k@oksbi&pn=ABDUL%40HADI&am='+price+'&cu=INR&aid=uGICAgICTvd6KIQ'
+    url = pyqrcode.create(s)
+    url.png(f"static/images/{filename}.png", scale=8)
+    file = os.path.join('static', 'images', f'{filename}.png')
+    
+    # deletes products.txt
+    text_file = os.path.join('storage', 'products.txt')
+    os.remove(text_file)
+    
+    return render_template('qr.html', image=file)
+    
+    
 if __name__ == '__main__':
     app.run(debug=False)
-  
-
 
 
